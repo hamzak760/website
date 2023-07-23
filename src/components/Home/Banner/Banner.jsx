fa26a1a4b383fcbcf2dc733da3c6ef54a4f3a966
hamzak760 <hamzak760@gmail.com>
2023-07-23 23:43:59 +0500

    header and banner complete

--- src/components/Home/Banner/Banner.jsx
+++ src/components/Home/Banner/Banner.jsx
@@ -1,6 +1,26 @@
 import "./Banner.scss";
+import BannerImg from "../../../assets/banner-img.png";
 const Banner = () => {
-    return <div>Banner</div>;
+  return (
+    <div className="hero-banner">
+      <div className="content">
+        <div className="text-content">
+          <h1>SALES</h1>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet
+            perferendis aperiam necessitatibus cum nobis distinctio aut harum
+            corrupti sit fuga fugit, quasi molestiae vitae quod, mollitia
+            tempore possimus inventore. Nemo!
+          </p>
+          <div className="ctas">
+            <div className="banner-cta">Read More</div>
+            <div className="banner-cta v2">Shop Now</div>
+          </div>
+        </div>
+        <img className="banner-img" src={BannerImg} alt="" />
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
