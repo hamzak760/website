defea04b080f86f9933a4ade6ff0ed434be328a5
hamzak760 <hamzak760@gmail.com>
2023-07-29 22:18:53 +0500

    api setup

---
+++ src/utils/api.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const params ={
+    headers:{
+        Authorization: "bearer" + ProcessingInstruction.evn.REACT_APP_STRIPE_APP_KEY,
+    }
+};
+
+export const fetchDataFromApi = async (url) => {
+    try{
+        const {data} = await axios.get(process.env.REACT_APP_DEV_URL + params);
+        return data
+    } catch (error) {
+        console.log(error);
+        return error;
+    }
+}
