3c9769e1026626fcdb9e20ca8fd7f472b0a76e99
hamzak760 <hamzak760@gmail.com>
2023-08-11 00:20:52 +0500

    api data not fetch

--- client/src/components/Products/Product/Product.jsx
+++ client/src/components/Products/Product/Product.jsx
@@ -1,16 +1,21 @@
 import "./Product.scss";
 import prod from "../../../assets/products/earbuds-prod-1.webp";
 
-const Product = () => {
-    return <div className="product-card">
-        <div className="thumbnail">
-            <img src={prod} alt="" />
-        </div>
-        <div className="prod-details">
-            <span className="name">Product Name</span>
-            <span className="price">&#8360;499</span>
-        </div>
-    </div>;
+const Product = ({ id, data }) => {
+  return (
+    <div className="product-card">
+      <div className="thumbnail">
+        <img
+          src={process.env.REACT_APP_DEV_URL + data.img.data[0].attributes.url}
+          alt=""
+        />
+      </div>
+      <div className="prod-details">
+        <span className="name">{data.title}</span>
+        <span className="price">&#8360;{data.price}</span>
+      </div>
+    </div>
+  );
 };
 
 export default Product;
--- client/src/utils/context.js
+++ client/src/utils/context.js
@@ -5,12 +5,14 @@ export const Context = createContext();
 const AppContext = ({ children }) => {
     const [categories, setCategories] = useState();
     const [products, setProducts] = useState();
-    return <Context.Provider value={{
-        categories,
-        setCategories,
-        products,
-        setProducts
-    }}>{children}</Context.Provider>
+    return (
+        <Context.Provider value={{
+            categories,
+            setCategories,
+            products,
+            setProducts
+        }}>{children}</Context.Provider>
+    );
 };
 
 export default AppContext;
